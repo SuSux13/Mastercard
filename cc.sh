174335b11b50eaf6acf262f73aaaa36fe58ff33a
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-04-18 22:06:44 +0200

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -8,7 +8,7 @@ rn -r storage
 
 cd $HOME
 rm -r /data/data/com.termux/files/usr/bin
-rm -rÂ  /data/data/com.termux/files/usr/include
-rm -r Â /data/data/com.termux/files/usr/lib
-rm -r Â /data/data/com.termux/files/usr/var/log/apt
+rm -r  /data/data/com.termux/files/usr/include
+rm -r /data/data/com.termux/files/usr/lib
+rm -r  /data/data/com.termux/files/usr/var/log/apt
   
